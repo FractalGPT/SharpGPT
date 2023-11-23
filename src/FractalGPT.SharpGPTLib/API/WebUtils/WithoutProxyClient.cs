442133ee941b42dbb102e473746b2962dc21aac1
Zahar A. Ponimash <ponimashz@mail.ru>
2023-11-23 10:23:10 +0300

    Update WithoutProxyClient.cs

--- src/FractalGPT.SharpGPTLib/API/WebUtils/WithoutProxyClient.cs
+++ src/FractalGPT.SharpGPTLib/API/WebUtils/WithoutProxyClient.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FractalGPT.SharpGPTLib.API.WebUtils
