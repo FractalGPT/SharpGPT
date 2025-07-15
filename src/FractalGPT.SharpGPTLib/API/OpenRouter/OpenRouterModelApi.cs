09c76a7d2b42f783b846d3397d396364e913a88c
Zahar A. Ponimash <ponimashz@mail.ru>
2025-07-15 05:16:26 +0300

    Create OpenRouterModelApi.cs

---
+++ src/FractalGPT.SharpGPTLib/API/OpenRouter/OpenRouterModelApi.cs
@@ -0,0 +1,19 @@
+﻿using FractalGPT.SharpGPTLib.API.LLMAPI;
+using FractalGPT.SharpGPTLib.Stream;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FractalGPT.SharpGPTLib.API.OpenRouter
+{
+    /// <summary>
+    /// Работа с API OpenRouter
+    /// </summary>
+    public class OpenRouterModelApi : ChatLLMApi
+    {
+        public OpenRouterModelApi(string key, string modelName, IStreamHandler streamSender = null, string prompt = "", double temperature = 0, bool useProxy = false, string proxyPath = null) : base(key, useProxy, proxyPath, modelName, prompt, temperature, streamSender)
+        {
+            ApiUrl = "https://openrouter.ai/api/v1/chat/completions";
+        }
+    }
+}
