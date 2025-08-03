51b633e408ab80b248481f0a82c4ea6f579dd18d
potaninmt <marat.potanin12@mail.ru>
2025-08-04 01:55:43 +0300

    IStreamHandler

--- src/FractalGPT.SharpGPTLib/Stream/IStreamHandler.cs
+++ src/FractalGPT.SharpGPTLib/Stream/IStreamHandler.cs
@@ -2,4 +2,8 @@
 public interface IStreamHandler
 {
     Task<string> StartStreamAsync(string streamId, HttpResponseMessage response);
+
+    Task<string> StartStreamAsync(string streamId, string message);
+
+    Task<string> StartStreamAsync<T>(string streamId, T message) where T: class;
 }
